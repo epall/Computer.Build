909555cecc0ac4aec3849ac5408f8270021ed323
Eric Allen <ericpallen@gmail.com>
2009-12-19 19:38:35 -0500

    Syntactically valid Ruby version of mux

---
+++ lib/multiplexer.rb
@@ -0,0 +1,19 @@
+entity "Mux" do |mux|
+  mux.port :I3, :in, "std_logic_vector(2 downto 0)"
+  mux.port :I2, :in, "std_logic_vector(2 downto 0)"
+  mux.port :I1, :in, "std_logic_vector(2 downto 0)"
+  mux.port :I0, :in, "std_logic_vector(2 downto 0)"
+  mux.port :S, :in, "std_logic_vector(1 downto 0)"
+  mux.port :O, :out, "std_logic_vector(2 downto 0)"
+  mux.behavior do |b|
+    b.process [:I3, :I2, :I1, :I0, :S] do |p|
+      p.case :S do |c|
+        c["00"] = assign :O, :I0
+        c["01"] = assign :O, :I1
+        c["10"] = assign :O, :I2
+        c["11"] = assign :O, :I3
+        c[:default] = assign :O, "ZZZ"
+      end
+    end
+  end
+end
