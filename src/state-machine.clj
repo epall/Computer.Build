40cc0e01f47d88ff04824a0656fb4af2ea87bc16
Eric Allen <ericpallen@gmail.com>
2010-01-28 10:50:23 -0800

    Successfully compiling state machine in Clojure
    It doesn't do a whole lot yet, but this is just a proof of concept.

--- src/state-machine.clj
+++ src/state-machine.clj
@@ -1,5 +1,6 @@
 (ns state-machine
-  (:use computer-build.vhdl clojure.contrib.pprint))
+  (:use computer-build.vhdl clojure.contrib.pprint)
+  (:refer-clojure :rename {:name :keyword-to-str}))
 
 (defn state-machine [name inputs outputs states transitions]
 	"Create a state machine that operates from the given inputs, triggering
@@ -10,12 +11,18 @@
 	`(entity ~(gensym name)
           ; Ports
           ~(concat ['(:clock :in "std_logic")] inports outports)
+          ; Definitions
+          ((deftype "STATE_TYPE" ~(map #(str "state_" (keyword-to-str %)) (keys states)))
+          (signal "state" "STATE_TYPE"))
           ; Behavior
-          (process ~(conj inputs :clock) ()))))
+          (process ~(conj inputs :clock) 
+                   (if (and (event :clock) (= :clock "'1'"))
+                     (case :state
+                           "state_on" (<= :bulb "1")))))))
 
 (def my-sm (state-machine "pushbutton" [:push] [:bulb]
-               {:on '(<= :bulb "1") :off '(<= :bulb "0")}
-               [
+               {:on '(<= :bulb "1") :off '(<= :bulb "0")} ; states
+               [ ; transitions
                 '(:on (= :push "1") :off)
                 '(:off (= :push "0") :on)
                 ]))
